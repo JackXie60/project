a8e5fb44d9f00c8605fb8ce83cb9e0f8e71ada68
JackXie60 <1809697652@qq.com>
2021-06-11 16:18:55 +0800

    响应式网页

--- src/api/URLS.js
+++
@@ -1,8 +0,0 @@
-const baseUrL = 'https://mallapi.duyiedu.com/goods/';
-export default baseUrL;
-export const Url = {
-  side: '/getsidebar',
-  goods: '/getGoodsList',
-  search: '/search',
-  likeSearch: 'likeSearch',
-};
